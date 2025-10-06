91db3bf4f96ed0b03ac9c67b42015ffc950a0481
Angel Alberto <zS25018141@estudiantes.uv.mx>
2025-10-05 20:49:42 -0600

    build 1.0

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int number1 = 0, number2 = 0;
     char simbol;
@@ -8,6 +9,23 @@ int main() {
     cin >> number1 >> number2;
     cout << "Ahora escribe tu signo de operacion (+, -, *, /): ";
     cin >> simbol;
-    cout << "numero 1: " << number1 << "\tnumero 2: " << number2 << "\tsigno: " << simbol;
+    cout << "numero 1: " << number1 << "\tnumero 2: " << number2 << "\tsigno: " << simbol << endl;
+    if (simbol == '+') {
+        result = number1 + number2;
+    } else if (simbol == '-') {
+        result = number1 - number2;
+    } else if (simbol == '*') {
+        result = number1 * number2;
+    } else if (simbol == '/') {
+        if (number2 == 0) {
+            cout << "Error: Division por cero." << endl;
+            return 1;
+        }
+        result = (float)number1 / number2;
+    } else {
+        cout << "Operacion invalida." << endl;
+        return 1;
+    }
+    cout << "El resultado es: " << result << endl;
+    return 0;
 }
-    
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Prácticas en c++
 ***
 ## Mi primer proyecto en c++
-Escribe dos numeros y el signo de operacion
-# Fase de desarrollo aún...
+### Angel Alberto Sanchez Ramirez
+### S25018141
+### 05/10/25
 ***
+## ¿Que hace?
+Este programa pide al usuario dos números, después
+un signo de operación y la realiza
+***
+## Ejemplo de funcionamiento
+![Ejemplo del programa](/images/ejemplo.png)
+---
+Aqui podemos ver que el código funciona de forma
+correcta
